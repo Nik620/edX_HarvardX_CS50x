f249e1698d9187f137ddfdbea9193f1b0b413774
Nik Paulic <62592843+Nik620@users.noreply.github.com>
2020-10-16 16:37:39 +0900

    Create mario.py

---
+++ PSET6/mario.py
@@ -0,0 +1,13 @@
+from cs50 import get_int
+
+
+def block_builder(n):
+    # empty space
+    if n < 1 or n > 8:
+        block_builder(get_int("Invalid, insert height: "))
+    # block
+    for i in range(1, n+1):
+        print(" "*(n-i)+"#"*i + "  " + "#"*i)
+    
+
+block_builder(get_int("Height: "))
