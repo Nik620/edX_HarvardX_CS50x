d859b8469a73775dffb42b123d112c2fb230aee2
Nik Paulic <62592843+Nik620@users.noreply.github.com>
2020-10-16 16:42:06 +0900

    Create 1.sql

---
+++ PSET7/movies/1.sql
@@ -0,0 +1,5 @@
+/*table with single column of movies*/
+/*all movies released in 2008*/
+SELECT title FROM movies WHERE year = 2008;
+/*COMPLETE to test:
+after title, write comma then COUNT(title)*/
